db5174d0b53ed5c18f9bd16772ca29663aacf5a3
snpe15 <snehaswetha.p@gmail.com>
2015-11-21 19:22:53 +0530

    feature 1 front end

---
+++ index.php
@@ -0,0 +1,101 @@
+
+<html>
+<head>
+<script src="admin/js/jquery.js"></script>
+<link rel="stylesheet" href="css/style1.css">
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+
+<title> Group 12 Shop</title>
+</head>
+<body>
+<!--main div starts-->
+
+<div id="main">
+<div id="main1">
+
+<!--inner div starts-->
+
+<div id="innermain">
+<img src="images/companylogo.png" width="100" height="80" style="position:relative; margin-left:10px;margin-top:15px">
+</div>
+<center>
+<h1 style="margin-top:-150px; margin-bottom:20px;font-family: 'Lobster', cursive; color:#00b1b7; font-size:75px;">Online Grocery Store</h1>
+</center>
+</div>
+
+<!--inner end starts-->
+
+<div id="nav">
+<ul>
+<center>
+<li><a href="index.php">Home</a></li>
+<li><a href="search.php">Search</a></li>
+<li><a href="coustmerreg.php">Register</a></li>
+<li><a href="coustmer1.php">Login</a></li>
+<li><a href="mycart.php">MyCart</a></li>
+</ul>
+</center>
+</div>
+<hr>
+<img src="images/background.jpg" width="100%" height="400px">
+
+
+<div id="nav"></div>
+<div id="left">
+<br>
+<center>
+<ul>
+<li style="color:black;font-weight:bold;font-family: 'Lobster', cursive;">SELECT STORE </li>
+<li style="background-color:#B2E7E9"><a href="categorybook.php?qs=health">Willys</a></li>
+<li><a href="categorybook.php?qs=fiction">ICA</a></li>
+<li style="background-color:#B2E7E9"><a href="categorybook.php?qs=science">CityGross</a></li>
+<li><a href="categorybook.php?qs=Computers">Netto</a></li>
+<li style="background-color:#B2E7E9"><a href="categorybook.php?qs=yoga">Hemköp</a></li>
+<li><a href="categorybook.php?qs=Computers">Maxi</a></li>
+<li style="background-color:#B2E7E9"><a href="categorybook.php?qs=yoga">Coop</a></li>
+<li><a href="categorybook.php?qs=Computers">Fruckt</a></li>
+<li style="background-color:#B2E7E9"><a href="categorybook.php?qs=yoga">FoodWorld</a></li>
+<li><a href="categorybook.php?qs=Computers">Ratnadeep</a></li>
+<li style="background-color:#B2E7E9"><a href="categorybook.php?qs=yoga">Heritage Fresh</a></li>
+</ul>
+<center>
+</div>
+<br>
+<div id="right">
+
+</div>
+<div id="right1" style="height:580px; margin-top:20px;" align="center">
+</div>
+
+
+
+
+</div>
+<!--main starts-->
+
+
+<!-- footer-->
+<div id="footer">
+<ul>
+<center>
+<Br>
+<br>
+<li><a href="">Privacy Policy</a></li>
+<li><a href="">Return Policy</a></li>
+<li><a href="contact.php">Contact Us</a></li>
+</ul>
+</center>
+<br>
+<center>
+<span style="color:white;align:center"> &copy; 2015 Group12Shop </span>
+</center>
+</div>
+
+<!--footer end-->
+
+</div>
+</body>
+
+
+
+</html>
