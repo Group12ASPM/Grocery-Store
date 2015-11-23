a37bca0b5685aadc78e08263e59380302966511f
anuchunduri11 <anuchunduri11@gmail.com>
2015-11-23 23:23:07 +0100

    Improvements to home page frontend

--- customer/index.php
+++ customer/index.php
@@ -1,16 +1,123 @@
-<?php
-    session_start();
-    if($_SESSION['username'])
-    {
-        echo "<script> location='coustmer/coustmerafterlogin.php' </script>";
-    }
-?>
+
 <html>
 <head>
-<script src="admin/js/jquery.js"></script>
+<script src="js/jquery.js"></script>
 <link rel="stylesheet" href="css/style1.css">
 <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet" type="text/css">
+<script>
+function p1showfunction() {
+    document.getElementById("p1div").style.zIndex = '1';
+    document.getElementById("p1hdiv").style.zIndex= '2';
+    document.getElementById("p1hdiv").style.display = 'initial';
+    document.getElementById("info1").style.display = 'initial';
+    
+}
+function p1hidefunction() {
+    document.getElementById("p1hdiv").style.display = 'none';
+    document.getElementById("info1").style.display = 'none';
+}
 
+function p2showfunction() {
+    document.getElementById("p2div").style.zIndex = '1';
+    document.getElementById("p2hdiv").style.zIndex= '2';
+    document.getElementById("p2hdiv").style.display = 'initial';
+    document.getElementById("info2").style.display = 'initial';
+    
+}
+function p2hidefunction() {
+    document.getElementById("p2hdiv").style.display = 'none';
+    document.getElementById("info2").style.display = 'none';
+    
+}
+
+function p3showfunction() {
+    document.getElementById("p3div").style.zIndex = '1';
+    document.getElementById("p3hdiv").style.zIndex= '2';
+    document.getElementById("p3hdiv").style.display = 'initial';
+    document.getElementById("info3").style.display = 'initial';
+    
+}
+function p3hidefunction() {
+    document.getElementById("p3hdiv").style.display = 'none';
+    document.getElementById("info3").style.display = 'none';
+    
+}
+function p4showfunction() {
+    document.getElementById("p4div").style.zIndex = '1';
+    document.getElementById("p4hdiv").style.zIndex= '2';
+    document.getElementById("p4hdiv").style.display = 'initial';
+    document.getElementById("info4").style.display = 'initial';
+    
+}
+function p4hidefunction() {
+    document.getElementById("p4hdiv").style.display = 'none';
+    document.getElementById("info4").style.display = 'none';
+    
+}
+function p5showfunction() {
+    document.getElementById("p5div").style.zIndex = '1';
+    document.getElementById("p5hdiv").style.zIndex= '2';
+    document.getElementById("p5hdiv").style.display = 'initial';
+    document.getElementById("info5").style.display = 'initial';
+    
+}
+function p5hidefunction() {
+    document.getElementById("p5hdiv").style.display = 'none';
+    document.getElementById("info5").style.display = 'none';
+    
+}
+function p6showfunction() {
+    document.getElementById("p6div").style.zIndex = '1';
+    document.getElementById("p6hdiv").style.zIndex= '2';
+    document.getElementById("p6hdiv").style.display = 'initial';
+    document.getElementById("info6").style.display = 'initial';
+    
+    
+}
+function p6hidefunction() {
+    document.getElementById("p6hdiv").style.display = 'none';
+    document.getElementById("info6").style.display = 'none';
+    
+}
+
+function p7showfunction() {
+    document.getElementById("p7div").style.zIndex = '1';
+    document.getElementById("p7hdiv").style.zIndex= '2';
+    document.getElementById("p7hdiv").style.display = 'initial';
+    document.getElementById("info7").style.display = 'initial';
+    
+}
+function p7hidefunction() {
+    document.getElementById("p7hdiv").style.display = 'none';
+    document.getElementById("info7").style.display = 'none';
+    
+}
+function p8showfunction() {
+    document.getElementById("p8div").style.zIndex = '1';
+    document.getElementById("p8hdiv").style.zIndex= '2';
+    document.getElementById("p8hdiv").style.display = 'initial';
+    document.getElementById("info8").style.display = 'initial';
+    
+}
+function p8hidefunction() {
+    document.getElementById("p8hdiv").style.display = 'none';
+    document.getElementById("info8").style.display = 'none';
+    
+}
+function p9showfunction() {
+    document.getElementById("p9div").style.zIndex = '1';
+    document.getElementById("p9hdiv").style.zIndex= '2';
+    document.getElementById("p9hdiv").style.display = 'initial';
+    document.getElementById("info9").style.display = 'initial';
+    
+}
+function p9hidefunction() {
+    document.getElementById("p9hdiv").style.display = 'none';
+    document.getElementById("info9").style.display = 'none';
+    
+}
+</script>
 <title> Group 12 Shop</title>
 </head>
 <body>
@@ -67,18 +174,103 @@
 <center>
 </div>
 <br>
-<div id="right">
-
-</div>
-<div id="right1" style="height:580px; margin-top:20px;" align="center">
+<div id="right"> </div>
 </div>
 
-
-
-
-</div>
+<br>
+<br>
 <!--main starts-->
+<section>
+<p style="text-align:center;margin-bottom:25px;margin-top:25px;font-weight:bold;font-size:25px"> POPULAR GROCERIES AND OFFERS FOR THE DAY </p>
+<Br>
+<br>
+<!-- Row 1-->
+<div id="row1">
 
+<div onmouseover="p1showfunction()" onmouseout="p1hidefunction()" id="p1div" style="width:30%;position:relative;margin-top:10px;margin-right:20px;display:inline-block">
+<div id="p1hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product1.jpg" style="height:250px;position:relative" width="100%" />
+<p align="center"> Fresh Brown Bread <br> <b>Price: </b> 25 kr </p>
+<p  id="info1" style="position:absolute;left:20%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW from Willys </p>
+</div>
+
+
+<div onmouseover="p2showfunction()" onmouseout="p2hidefunction()" id="p2div" style="width:30%;margin-top:10px;display:inline-block;margin-right:20px;position:relative">
+<div id="p2hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;width:100%;height:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product2.jpg" style="height:250px" width="100%"/>
+<p align="center"> <span style="color:Red">Buy one get one free</span> OLW Chips <br> <b>Price: </b> 18 kr </p>
+<p  id="info2" style="position:absolute;left:24%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW from ICA </p>
+</div>
+
+
+<div onmouseover="p3showfunction()" onmouseout="p3hidefunction()" id="p3div" style="width:30%;margin-top:10px;display:inline-block;position:relative">
+<div id="p3hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product3.jpg" style="height:250px" width="100%"/>
+<p align="center"> Marmalade Oranges 1 Kg Pack <br> <b>Price: </b> 19 kr </p>
+<p  id="info3" style="position:absolute;left:26%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW  from Willys</p>
+</div>
+
+
+</div>
+
+<!-- Row 2-->
+<div id="row2">
+
+<div onmouseover="p4showfunction()" onmouseout="p4hidefunction()" id="p4div" style="width:30%;margin-top:10px;display:inline-block;margin-right:20px;position:relative">
+<div id="p4hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product4.jpg" style="height:250px" width="100%"/>
+<p align="center"> JANES Crispy Chicken Wings <br> <b>Price: </b> 49 kr </p>
+<p  id="info4" style="position:absolute;left:24%;top:50%;color:#00FFC9;display:none;z-index:3">Click to BUY NOW from Hemköp </p>
+</div>
+
+
+<div onmouseover="p5showfunction()" onmouseout="p5hidefunction()" id="p5div" style="width:30%;margin-top:10px;display:inline-block;margin-right:25px;position:relative">
+<div id="p5hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product5.jpg" style="height:250px" width="100%"/>
+<p align="center"> Celeste Pizza for One <br> <b>Price: </b> 19.50 kr </p>
+<p  id="info5" style="position:absolute;left:26%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW  from Lidl </p>
+</div>
+
+
+<div onmouseover="p6showfunction()" onmouseout="p6hidefunction()" id="p6div" style="width:25%;margin-top:10px;display:inline-block;margin-right:20px;position:relative">
+<div id="p6hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product6.jpg"  style="height:250px" width="100%"/>
+<p align="center"> Fresh Organic Strawberries <br> <b>Price: </b> 40 kr </p>
+<p  id="info6" style="position:absolute;left:24%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW  from Coop </p>
+</div>
+</div>
+
+<!--Row 3-->
+<div id="row3">
+
+<div onmouseover="p7showfunction()" onmouseout="p7hidefunction()" id="p7div" style="width:30%;margin-top:10px;display:inline-block;margin-right:20px;position:relative">
+<div id="p7hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product7.jpg" style="height:250px" width="100%"/>
+<p align="center"> <span style="color:red">NEW! </span>
+Kellogg's Crunchy Nut<br> <b>Price: </b> 54 kr </p>
+<p  id="info7" style="position:absolute;left:26%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW  from Lidl</p>
+</div>
+
+
+<div onmouseover="p8showfunction()" onmouseout="p8hidefunction()" id="p8div" style="width:30%;margin-top:10px;display:inline-block;margin-right:20px;position:relative">
+<div id="p8hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product8.jpg" style="height:250px" width="100%"/>
+<p align="center"> Kungsörnen Pancake Mix <br> <b>Price: </b> 35 kr </p>
+<p  id="info8" style="position:absolute;left:24%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW  from Fruckt </p>
+</div>
+
+
+<div onmouseover="p9showfunction()" onmouseout="p9hidefunction()" id="p9div" style="width:30%;margin-top:10px;display:inline-block;margin-right:20px;position:relative">
+<div id="p9hdiv" style="position:absolute;top:0;left:0;background-color:black;opacity:0.4;height:100%;width:100%;display:none;border: 5px solid #33A1C9;"></div>
+<img src="images/product9.jpg" style="height:250px" width="100%"/>
+<p align="center"> TESCO Organic Bananas <br> <b>Price: </b> 19 kr </p>
+<p  id="info9" style="position:absolute;left:26%;top:50%;color:#00FFC9;display:none;z-index:3"> Click to BUY NOW  from Willys </p>
+</div>
+</div>
+</section>
+
+<br>
+<br>
 
 <!-- footer-->
 <div id="footer">
