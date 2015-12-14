9a240d267ff94a8fe9f14a16e20ee9a4f04e85c5
sharenpolavarapu <shpo15@student.bth.se>
2015-12-14 18:23:45 +0100

    mycart backend

---
+++ customer/mycart-wfruits.php
@@ -0,0 +1,23 @@
+<?php
+        include "conn.php";
+        if(isset($_POST['add']))
+        {
+            $prodid = $_GET['pid'];
+            $prod_quant = $_POST['quant'];
+            $userid = 1;
+            $cartid = 1;
+            $storeid = $_GET['sid'];
+        }
+        $data= "INSERT INTO MyCart VALUES ('$cartid','$prod_quant','$userid','$prodid','$storeid')";
+        
+        if(mysql_query($data))
+        {
+            echo"<script>alert('Product Successfully added to your Cart!');</script>";
+            echo"<script>location='willysfruit.php?yon=yes&id=".$prodid."'</script>";
+        }
+        else
+        {
+            echo"<script>alert('Product already placed in your Cart!');</script>";
+            echo"<script>location='willysfruit.php?yon=no&id=".$prodid."'</script>";
+        }
+?>
