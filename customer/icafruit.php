ff462809dafe6188ab7c440c2be3c577e5bd5e31
snpe15 <snehaswetha.p@gmail.com>
2015-12-14 22:26:35 +0530

    Product page ica front end

---
+++ customer/icafruit.php
@@ -0,0 +1,109 @@
+
+<html>
+<head>
+<script src="js/jquery.js"></script>
+<link rel="stylesheet" href="css/style1.css">
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet" type="text/css">
+
+<title> Group 12 Shop</title>
+</head>
+<body>
+<!--main div starts-->
+
+<div id="main">
+<div id="main1">
+
+<!--inner div starts-->
+
+<div id="innermain">
+<img src="images/companylogo.png" width="100" height="80" style="position:relative; margin-left:10px;margin-top:15px">
+</div>
+<center>
+<h1 style="margin-top:-150px; margin-bottom:20px;font-family: 'Lobster', cursive; color:#00b1b7; font-size:75px;">Online Grocery Store - ICA </h1>
+</center>
+</div>
+
+<!--inner end starts-->
+
+<div id="nav">
+<ul>
+<center>
+<li><a href="index.php">Home</a></li>
+<li><a href="search.php">Search</a></li>
+<li><a href="coustmerreg.php">Register</a></li>
+<li><a href="coustmer1.php">Login</a></li>
+<li><a href="mycart.php">MyCart</a></li>
+</ul>
+</center>
+</div>
+<hr>
+<img src="images/background.jpg" width="100%" height="400px">
+
+
+<div id="nav"></div>
+<div id="left">
+<br>
+<center>
+<ul>
+<li style="color:black;font-weight:bold;font-family: 'Lobster', cursive;">SELECT STORE </li>
+<li style="background-color:#B2E7E9;"><a href="willys.php" >Willys</a></li>
+<li style="border:5px solid black"><a href="ica.php" style="color:Red">ICA</a></li>
+<li style="background-color:#B2E7E9"><a href="">CityGross</a></li>
+<li><a href="">Coop</a></li>
+<li style="background-color:#B2E7E9"><a href="">Hemköp</a></li>
+
+</ul>
+<center>
+</div>
+<br>
+<div id="right"></div>
+</div>
+
+<!--main starts-->
+
+<!--left-->
+
+<div style="background:#000080;color:white;display:inline-block;width:18%;">
+<a href="ica.php" style="text-decoration:none;"><span style="color:white;font-weight:bold;font-family: 'Lobster', cursive;font-size:30px;">Back to List</span></a>
+</div>
+
+<!--right for fruits-->
+
+<p style="text-align:center;margin-bottom:25px;margin-top:25px;font-weight:bold;font-size:25px"> Welcome to <span style="color:red;font-weight:bold"> ICA </span> Online - Fruits</p>
+<br>
+
+<!--row for display-->
+<div style="width:70%;margin:auto">
+<!-- code from backend-->
+</div>
+
+
+
+
+<!-- footer-->
+<div id="footer">
+<ul>
+<center>
+<Br>
+<br>
+<li><a href="privacy.php">Privacy Policy</a></li>
+<li><a href="returnpolicy.php">Return Policy</a></li>
+<li><a href="contact.php">Contact Us</a></li>
+</ul>
+</center>
+<br>
+<center>
+<span style="color:white;align:center"> &copy; 2015 Group12Shop </span>
+</center>
+</div>
+
+
+<!--footer end-->
+
+</div>
+</body>
+
+
+
+</html>
